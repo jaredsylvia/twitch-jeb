afb4ffc51eb28812044274680bd14330d0cb932f
Jared Sylvia <jaredsylvia@gmail.com>
2023-10-03 19:22:39 -0700

    Add files via upload
    started actually doing stuff

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ let twitchBotClient = null;
 // Create an Express app
 const app = express();
 
+// Set up variables
+let flipping = false;
+let heads = [];
+let tails = [];
+let winningCoin = '';
+
 // Define a route for the home page
 app.get('/', (req, res) => {
     res.send('Hello, world!');
@@ -88,9 +94,10 @@ app.get('/auth/twitch/callback', async (req, res) => {
     twitchBotClient.on('subscription', onTwitchBotSubscriptionHandler);
     twitchBotClient.on('subgift', onTwitchBotSubgiftHandler);
     twitchBotClient.on('submysterygift', onTwitchBotSubmysterygiftHandler);
-    
 
-
+    //Store current chat stats in variables
+    //let currentSubs = twitchBotClient.getChannels()[0].subscriberCount;
+    //let currentFollowers = twitchBotClient.getChannels()[0].followerCount;
 
     // Redirect to the home page
     res.redirect('/');
@@ -105,137 +112,15 @@ app.listen(serverPort, serverIp, () => {
 //Event handler functions
 function onTwitchBotMessageHandler(channel, userstate, message, self) {
     // Check if command
-    if (message.startsWith('!') && (userstate.badges.broadcaster === '1' || userstate.mod === true)) { // Commands for moderators
-        const args = message.slice(1).split(' ');
-        const command = args.shift().toLowerCase();
-
-        switch (command) {
-            case 'ping':
-                twitchBotClient.say(channel, 'Pong!');
-                break;
-            case 'pong':
-                twitchBotClient.say(channel, 'Ping!');
-                break;
-            case 'uptime':
-                twitchBotClient.say(channel, 'Uptime!');
-                break;
-            case 'parrot':
-                twitchBotClient.say(channel, message.substring(7));
-                break;
-            case 'ban':
-                twitchBotClient.ban(channel, message.substring(5));
-                break;
-            case 'unban':
-                twitchBotClient.unban(channel, message.substring(7));
-                break;
-            case 'timeout':
-                twitchBotClient.timeout(channel, message.substring(9));
-                break;
-            case 'untimeout':
-                twitchBotClient.untimeout(channel, message.substring(11));
-                break;
-            case 'slow':
-                twitchBotClient.slow(channel, message.substring(6));
-                break;
-            case 'slowoff':
-                twitchBotClient.slowoff(channel);
-                break;
-            case 'followers':
-                twitchBotClient.followersonly(channel, message.substring(10));
-                break;
-            case 'followersoff':
-                twitchBotClient.followersonlyoff(channel);
-                break;
-            case 'clear':
-                twitchBotClient.clear(channel);
-                break;
-            case 'emoteonly':
-                twitchBotClient.emoteonly(channel);
-                break;
-            case 'emoteonlyoff':
-                twitchBotClient.emoteonlyoff(channel);
-                break;
-            case 'subscribers':
-                twitchBotClient.subscribers(channel);
-                break;
-            case 'subscribersoff':
-                twitchBotClient.subscribersoff(channel);
-                break;
-            case 'commercial':
-                twitchBotClient.commercial(channel, message.substring(11));
-                break;
-            case 'host':
-                twitchBotClient.host(channel, message.substring(6));
-                break;
-            case 'unhost':
-                twitchBotClient.unhost(channel);
-                break;
-            case 'raid':
-                twitchBotClient.raid(channel, message.substring(6));
-                break;
-            case 'modhelp':
-                switch (args[0]) {
-                    case 'ban':
-                        twitchBotClient.say(channel, 'Usage: !ban <username>');
-                        break;
-                    case 'unban':
-                        twitchBotClient.say(channel, 'Usage: !unban <username>');
-                        break;
-                    case 'timeout':
-                        twitchBotClient.say(channel, 'Usage: !timeout <username> <seconds>');
-                        break;
-                    case 'untimeout':
-                        twitchBotClient.say(channel, 'Usage: !untimeout <username>');
-                        break;
-                    case 'slow':
-                        twitchBotClient.say(channel, 'Usage: !slow <seconds>');
-                        break;
-                    case 'slowoff':
-                        twitchBotClient.say(channel, 'Usage: !slowoff');
-                        break;
-                    case 'followers':
-                        twitchBotClient.say(channel, 'Usage: !followers <minutes>');
-                        break;
-                    case 'followersoff':
-                        twitchBotClient.say(channel, 'Usage: !followersoff');
-                        break;
-                    case 'clear':
-                        twitchBotClient.say(channel, 'Usage: !clear');
-                        break;
-                    case 'emoteonly':
-                        twitchBotClient.say(channel, 'Usage: !emoteonly');
-                        break;
-                    case 'emoteonlyoff':
-                        twitchBotClient.say(channel, 'Usage: !emoteonlyoff');
-                        break;
-                    case 'subscribers':
-                        twitchBotClient.say(channel, 'Usage: !subscribers');
-                        break;
-                    case 'subscribersoff':
-                        twitchBotClient.say(channel, 'Usage: !subscribersoff');
-                        break;
-                    case 'commercial':
-                        twitchBotClient.say(channel, 'Usage: !commercial <length>');
-                        break;
-                    case 'host':
-                        twitchBotClient.say(channel, 'Usage: !host <channel>');
-                        break;
-                    case 'unhost':
-                        twitchBotClient.say(channel, 'Usage: !unhost');
-                        break;
-                    case 'raid':
-                        twitchBotClient.say(channel, 'Usage: !raid <channel>');
-                        break;
-                    default:
-                        twitchBotClient.say(channel, 'Commands: !ban, !unban, !timeout, !untimeout, !slow, !slowoff, !followers, !followersoff, !clear, !emoteonly, !emoteonlyoff, !subscribers, !subscribersoff, !commercial, !host, !unhost, !raid');
-                        break;
-                }
-                break;
-        }
-    } else if (message.startsWith('!')) { // Commands for non moderators
-        const args = message.slice(1).split(' ');
-        const command = args.shift().toLowerCase();
-
+    const args = message.slice(1).split(' ');
+    const command = args.shift().toLowerCase();
+    const isBroadcaster = userstate.badges && typeof userstate.badges.broadcaster !== 'undefined';
+    const isMod = userstate.mod === true;
+    console.log(`* ${userstate.username}: ${message}`);
+    console.log(`User is broadcaster: ${isBroadcaster}`);
+    console.log(`User is mod: ${isMod}`);
+    
+    if (message.startsWith('!')) { // Commands for non moderators
         switch (command) {
             case 'slap':
                 twitchBotClient.say(channel, `@${userstate.username} slapped ${message.substring(6)} with a large trout!`);
@@ -247,30 +132,229 @@ function onTwitchBotMessageHandler(channel, userstate, message, self) {
                 twitchBotClient.say(channel, `@${userstate.username} is no longer lurking!`);
                 break;
             case 'flip':
-                var flipping = true;
-                let heads = [];
-                let tails = [];
+                flipping = true;
+                
 
                 twitchBotClient.say(channel, `@${userstate.username} wants to flip a coin! Type !heads or !tails to guess!`);
                 //Give people time to respond using !heads or !tails
                 setTimeout(function () {
-                    twitchBotClient.say(channel, `Flipping a coin!`);
+                    
                     setTimeout(function () {
-                        twitchBotClient.say(channel, `The coin landed on ${Math.random() < 0.5 ? 'heads' : 'tails'}!`);
-                    }, 2000);
+                        twitchBotClient.say(channel, `Flipping a coin!`);
+                        //set winning coin
+                        winningCoin = Math.random() < 0.5 ? 'heads' : 'tails';
+                        //announce winning coin
+                        twitchBotClient.say(channel, `The coin landed on ${winningCoin}!`);
+                    }, 20000);
+                //check who guessed correctly
+                    setTimeout(function () {
+                        if(winningCoin === 'heads') {
+                            twitchBotClient.say(channel, `The following people guessed correctly: ${heads}`);
+                            twitchBotClient.say(channel, `The following people guessed incorrectly: ${tails}`);
+                            twitchBotClient.say(channel, `@${heads[0]} was the first winner!`);
+                            twitchBotClient.say(channel, `@${tails[0]} is the biggest loser!`);
+                        } else {
+                            twitchBotClient.say(channel, `The following people guessed correctly: ${tails}`);
+                            twitchBotClient.say(channel, `The following people guessed incorrectly: ${heads}`);
+                            twitchBotClient.say(channel, `@${tails[0]} was the first winner!`);
+                            twitchBotClient.say(channel, `@${heads[0]} is the biggest loser!`);
+                        }
+                        //reset variables
+                        heads = [];
+                        tails = [];
+                        winningCoin = '';
+                        flipping = false;
+                    }, 22000);
+                
 
                 }, 2000);
                 
                 break;
             case 'heads':
-                twitchBotClient.say(channel, `@${userstate.username} guessed heads!`);
-                //store the guess in an array
-
-                break;
-                
-
-
+                if(flipping) {
+                    //Check if user is already in heads array
+                    if(heads.includes(userstate.username) || tails.includes(userstate.username)) { 
+                        twitchBotClient.say(channel, `@${userstate.username} you already guessed!`);
+                        break;
                     }
+                    else {
+                        heads.push(userstate.username);
+                        twitchBotClient.say(channel, `@${userstate.username} guessed heads!`);
+                    }
+                    
+                } else {
+                    twitchBotClient.say(channel, `@${userstate.username} there is no coin to flip!`);
+                }
+                
+                break;
+            case 'tails':
+                if(flipping) {
+                    //Check if user is already in tails array
+                    if(heads.includes(userstate.username) || tails.includes(userstate.username)) { 
+                        twitchBotClient.say(channel, `@${userstate.username} you already guessed!`);
+                        break;
+                    }
+                    else {
+                        tails.push(userstate.username);
+                        twitchBotClient.say(channel, `@${userstate.username} guessed tails!`);
+                    }
+                    
+                } else {
+                    twitchBotClient.say(channel, `@${userstate.username} there is no coin to flip!`);
+                }
+                
+                break;
+            case 'help':
+                switch (args[0]) {
+                    case 'slap':
+                        twitchBotClient.say(channel, 'Usage: !slap <username>');
+                        break;
+                    case 'lurk':
+                        twitchBotClient.say(channel, 'Usage: !lurk');
+                        break;
+                    case 'unlurk':
+                        twitchBotClient.say(channel, 'Usage: !unlurk');
+                        break;
+                    case 'flip':
+                        twitchBotClient.say(channel, 'Usage: !flip');
+                        break;
+                    case 'heads':
+                        twitchBotClient.say(channel, 'Usage: !heads');
+                        break;
+                    case 'tails':
+                        twitchBotClient.say(channel, 'Usage: !tails');
+                        break;
+                    default:
+                        twitchBotClient.say(channel, 'Commands: !slap, !lurk, !unlurk, !flip, !heads, !tails');
+                        break;
+                }
+        }
+        if (message.startsWith('!') && (isMod || isBroadcaster)) { // Commands for moderators
+    
+            switch (command) {
+                case 'ping':
+                    twitchBotClient.say(channel, 'Pong!');
+                    break;
+                case 'pong':
+                    twitchBotClient.say(channel, 'Ping!');
+                    break;
+                case 'uptime':
+                    twitchBotClient.say(channel, 'Uptime!');
+                    break;
+                case 'parrot':
+                    twitchBotClient.say(channel, message.substring(7));
+                    break;
+                case 'ban':
+                    twitchBotClient.ban(channel, message.substring(5));
+                    break;
+                case 'unban':
+                    twitchBotClient.unban(channel, message.substring(7));
+                    break;
+                case 'timeout':
+                    twitchBotClient.timeout(channel, message.substring(9));
+                    break;
+                case 'untimeout':
+                    twitchBotClient.untimeout(channel, message.substring(11));
+                    break;
+                case 'slow':
+                    twitchBotClient.slow(channel, message.substring(6));
+                    break;
+                case 'slowoff':
+                    twitchBotClient.slowoff(channel);
+                    break;
+                case 'followers':
+                    twitchBotClient.followersonly(channel, message.substring(10));
+                    break;
+                case 'followersoff':
+                    twitchBotClient.followersonlyoff(channel);
+                    break;
+                case 'clear':
+                    twitchBotClient.clear(channel);
+                    break;
+                case 'emoteonly':
+                    twitchBotClient.emoteonly(channel);
+                    break;
+                case 'emoteonlyoff':
+                    twitchBotClient.emoteonlyoff(channel);
+                    break;
+                case 'subscribers':
+                    twitchBotClient.subscribers(channel);
+                    break;
+                case 'subscribersoff':
+                    twitchBotClient.subscribersoff(channel);
+                    break;
+                case 'commercial':
+                    twitchBotClient.commercial(channel, message.substring(11));
+                    break;
+                case 'host':
+                    twitchBotClient.host(channel, message.substring(6));
+                    break;
+                case 'unhost':
+                    twitchBotClient.unhost(channel);
+                    break;
+                case 'raid':
+                    twitchBotClient.raid(channel, message.substring(6));
+                    break;
+                case 'modhelp':
+                    switch (args[0]) {
+                        case 'ban':
+                            twitchBotClient.say(channel, 'Usage: !ban <username>');
+                            break;
+                        case 'unban':
+                            twitchBotClient.say(channel, 'Usage: !unban <username>');
+                            break;
+                        case 'timeout':
+                            twitchBotClient.say(channel, 'Usage: !timeout <username> <seconds>');
+                            break;
+                        case 'untimeout':
+                            twitchBotClient.say(channel, 'Usage: !untimeout <username>');
+                            break;
+                        case 'slow':
+                            twitchBotClient.say(channel, 'Usage: !slow <seconds>');
+                            break;
+                        case 'slowoff':
+                            twitchBotClient.say(channel, 'Usage: !slowoff');
+                            break;
+                        case 'followers':
+                            twitchBotClient.say(channel, 'Usage: !followers <minutes>');
+                            break;
+                        case 'followersoff':
+                            twitchBotClient.say(channel, 'Usage: !followersoff');
+                            break;
+                        case 'clear':
+                            twitchBotClient.say(channel, 'Usage: !clear');
+                            break;
+                        case 'emoteonly':
+                            twitchBotClient.say(channel, 'Usage: !emoteonly');
+                            break;
+                        case 'emoteonlyoff':
+                            twitchBotClient.say(channel, 'Usage: !emoteonlyoff');
+                            break;
+                        case 'subscribers':
+                            twitchBotClient.say(channel, 'Usage: !subscribers');
+                            break;
+                        case 'subscribersoff':
+                            twitchBotClient.say(channel, 'Usage: !subscribersoff');
+                            break;
+                        case 'commercial':
+                            twitchBotClient.say(channel, 'Usage: !commercial <length>');
+                            break;
+                        case 'host':
+                            twitchBotClient.say(channel, 'Usage: !host <channel>');
+                            break;
+                        case 'unhost':
+                            twitchBotClient.say(channel, 'Usage: !unhost');
+                            break;
+                        case 'raid':
+                            twitchBotClient.say(channel, 'Usage: !raid <channel>');
+                            break;
+                        default:
+                            twitchBotClient.say(channel, 'Commands: !ban, !unban, !timeout, !untimeout, !slow, !slowoff, !followers, !followersoff, !clear, !emoteonly, !emoteonlyoff, !subscribers, !subscribersoff, !commercial, !host, !unhost, !raid');
+                            break;
+                    }
+                    break;
+            }
+        }
         
     }
 }
